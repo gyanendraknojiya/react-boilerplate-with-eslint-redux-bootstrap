92afb1b11ccc254b2e79cc68f2bd750a93f63e5d
Gyanendra Knojiya <gyanendrak064@gmail.com>
2021-11-07 00:32:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, addAmount } from './redux/counterSlice';
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const amount = useSelector((state) => state.counterReducer.amount);
+
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="text-center">
+        <h1>React JS boilerplate</h1>
+        <h6>
+          By{' '}
+          <a href="https://github.com/gyanendraknojiya" target="_blank" without rel="noreferrer">
+            Gyanendra Knojiya
+          </a>
+        </h6>
+        <div className="display-2 my-3">{amount}</div>
+        <div className="my-4">
+          <button className="btn btn-success" onClick={() => dispatch(increment())}>
+            + Increment
+          </button>
+          <button className="btn btn-danger ms-2" onClick={() => dispatch(decrement())}>
+            + Decrement
+          </button>
+        </div>
+        <div>
+          <input
+            type="number"
+            className="form-control"
+            value={value}
+            onChange={(e) => setValue(parseInt(e.target.value))}
+          />
+          <div className="text-center">
+            <button
+              className="btn btn-primary  mt-2"
+              onClick={() => {
+                dispatch(addAmount(value));
+                setValue(0);
+              }}
+            >
+              ADD
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
